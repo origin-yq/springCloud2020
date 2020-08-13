ae2c83b15d44187668e064f693857a90bb8d01ff
yuanqiang <3296887716@qq.com>
2020-08-13 08:32:56 +0800

    spring cloud 提交通用provier工程

---
+++ microservicecloud-provider-dept/src/main/java/com/personal/springcloud/DeptProvider8081App.java
@@ -0,0 +1,11 @@
+package com.personal.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptProvider8081App {
+    public static void main(String[] args){
+        SpringApplication.run(DeptProvider8081App.class,args);
+    }
+}
