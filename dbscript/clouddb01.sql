ea518ffd4c9c1a456e2cab3f4cf8f6984d4b72cc
yuanqiang <3296887716@qq.com>
2020-08-15 21:07:54 +0800

    提交脚本

---
+++ dbscript/clouddb01.sql
@@ -0,0 +1,38 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80021
+ Source Host           : localhost:3306
+ Source Schema         : clouddb01
+
+ Target Server Type    : MySQL
+ Target Server Version : 80021
+ File Encoding         : 65001
+
+ Date: 15/08/2020 21:05:11
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for dept
+-- ----------------------------
+DROP TABLE IF EXISTS `dept`;
+CREATE TABLE `dept`  (
+  `deptNo` bigint(0) NOT NULL AUTO_INCREMENT,
+  `deptName` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `db_source` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`deptNo`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of dept
+-- ----------------------------
+INSERT INTO `dept` VALUES (1, '开发部', 'clouddb01');
+INSERT INTO `dept` VALUES (2, '人事部', 'clouddb01');
+INSERT INTO `dept` VALUES (3, '市场部', 'clouddb01');
+
+SET FOREIGN_KEY_CHECKS = 1;
--- microservicecloud-provider-dept/src/main/java/com/personal/springcloud/DeptProvider8081App.java
+++ microservicecloud-provider-dept/src/main/java/com/personal/springcloud/DeptProvider8081App.java
@@ -2,8 +2,20 @@ package com.personal.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+/**
+ * Description:
+ *
+ * Cannot determine embedded database driver class for database type NONE
+ *
+ * Action:
+ *
+ * If you want an embedded database please put a supported one on the classpath.
+ * If you have database settings to be loaded from a particular profile you may need to active it (no profiles are currently active).
+ * 解决该报错是排除默认的数据源配置 exclude = {DataSourceAutoConfiguration.class}
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DeptProvider8081App {
     public static void main(String[] args){
         SpringApplication.run(DeptProvider8081App.class,args);
