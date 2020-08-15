5ac4c3264783a8377e13604582543dc150215dc6
yuanqiang <3296887716@qq.com>
2020-08-15 23:16:52 +0800

    启动提供者

--- microservicecloud-provider-dept/src/main/java/com/personal/springcloud/DeptProvider8081App.java
+++ microservicecloud-provider-dept/src/main/java/com/personal/springcloud/DeptProvider8081App.java
@@ -1,10 +1,13 @@
 package com.personal.springcloud;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
+ * springboot启动时会自动注入数据源和配置jpa,如果spring配置文件没有添加数据库连接信息的配置会发生下面的报错信息
+ *
  * Description:
  *
  * Cannot determine embedded database driver class for database type NONE
@@ -13,9 +16,19 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  *
  * If you want an embedded database please put a supported one on the classpath.
  * If you have database settings to be loaded from a particular profile you may need to active it (no profiles are currently active).
- * 解决该报错是排除默认的数据源配置 exclude = {DataSourceAutoConfiguration.class}
+ * 解决办法：在@SpringBootApplication中排除其注入
+ * @SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+ *
+ * 但是如果配置了数据库连接信息，去掉@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})，还是会报上面的错
+ * 解决方法：pom依赖里加上即可，原理为止
+ * <dependency>
+ *             <groupId>com.h2database</groupId>
+ *             <artifactId>h2</artifactId>
+ *             <scope>runtime</scope>
+ *  </dependency>
  */
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@MapperScan(value ="com.personal.springcloud.dao" )
 public class DeptProvider8081App {
     public static void main(String[] args){
         SpringApplication.run(DeptProvider8081App.class,args);
--- microservicecloud-provider-dept/src/main/java/com/personal/springcloud/dao/DeptDao.java
+++ microservicecloud-provider-dept/src/main/java/com/personal/springcloud/dao/DeptDao.java
@@ -2,12 +2,13 @@ package com.personal.springcloud.dao;
 
 import com.personal.springcloud.entities.Dept;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Mapper
-public interface DeptDao {
-
+//Property 'sqlSessionFactory' or 'sqlSessionTemplate' are required
+@Repository   //@Mapper 这两个注解作用一样，想要生效，能够被注入为bean，需要在启动类添加@MapperScan注解
+public interface DeptDao{
     public boolean addDept(Dept dept);
     public Dept findById(Long id);
     public List<Dept> findAll();
