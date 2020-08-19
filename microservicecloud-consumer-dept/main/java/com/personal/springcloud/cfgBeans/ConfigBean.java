979c0809d1d7a16b531498b5f8f106ae7d21ffc0
yuanqiang <3296887716@qq.com>
2020-08-19 23:52:55 +0800

    ribbon 客户端的负载均衡，ribbon集成eureka实现consumer调用provider

--- microservicecloud-consumer-dept/main/java/com/personal/springcloud/cfgBeans/ConfigBean.java
+++ microservicecloud-consumer-dept/main/java/com/personal/springcloud/cfgBeans/ConfigBean.java
@@ -1,5 +1,6 @@
 package com.personal.springcloud.cfgBeans;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class ConfigBean {
 
     @Bean
+    @LoadBalanced   // 客户端的负载均衡,我们使用RestTemplate调用服务，通过@LoadBalanced注解[负载均衡]实现RestTemplate算法调用服务
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
--- microservicecloud-consumer-dept/main/java/com/personal/springcloud/controller/DeptConsumerController.java
+++ microservicecloud-consumer-dept/main/java/com/personal/springcloud/controller/DeptConsumerController.java
@@ -13,7 +13,9 @@ import java.util.List;
 @RestController
 public class DeptConsumerController {
 
-    private static final String REST_URL_PREFIX = "http://localhost:8081/";
+    // private static final String REST_URL_PREFIX = "http://localhost:8081/";
+    //microservicecloud-dept 服务提供者 对外 暴露的 服务名  ， ribbon 和 eureka 整合后consumer可以直接调用服务[即通过服务名]而不用关系服务地址和端口了
+    private static final String REST_URL_PREFIX = "http://microservicecloud-dept/";
     /**
      * url , 请求参数 ，返回封装类
      * postForObject(URI var1, Object var2, Class<T> var3)
