3cb23906a842f179b57a7e64f13d09a126431848
yuanqiang <3296887716@qq.com>
2020-08-20 23:43:29 +0800

    RandomRule() 随机算法测试，当我们显示地申明了负载均衡算法，则会覆盖掉默认的轮询算法

--- microservicecloud-consumer-dept/main/java/com/personal/springcloud/cfgBeans/ConfigBean.java
+++ microservicecloud-consumer-dept/main/java/com/personal/springcloud/cfgBeans/ConfigBean.java
@@ -1,5 +1,7 @@
 package com.personal.springcloud.cfgBeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +11,14 @@ import org.springframework.web.client.RestTemplate;
 public class ConfigBean {
 
     @Bean
-    @LoadBalanced   // 客户端的负载均衡[默认是轮询],我们使用RestTemplate调用服务，通过@LoadBalanced注解[负载均衡]实现RestTemplate算法调用服务
+    @LoadBalanced   // 客户端的负载均衡[默认是轮询RoundRobinRule()],我们使用RestTemplate调用服务，通过@LoadBalanced注解[负载均衡]实现RestTemplate算法调用服务
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
+    }
+
+    //当我们显示申明了负载均衡的算法，则会覆盖掉默认的轮询算法
+    @Bean
+    public IRule myRule(){
+        return new RandomRule();   //负载均衡的又一种算法：随机
     }
 }
