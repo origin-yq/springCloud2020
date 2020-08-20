bbb31b724ee602c2ce15352e8d668b88cc5290b1
yuanqiang <3296887716@qq.com>
2020-08-20 23:53:20 +0800

    RetryRule 重试算法测试，服务实例都正常，该算法与轮询算法一样，依次调用服务实例；当某一个实例宕机，那么开始还是正常依次调用所有实例，调到宕机的服务肯定报错，这样反复几次后，该算法就会排除掉宕机的服务，在剩余正常的服务实例中做轮询调用

--- microservicecloud-consumer-dept/main/java/com/personal/springcloud/cfgBeans/ConfigBean.java
+++ microservicecloud-consumer-dept/main/java/com/personal/springcloud/cfgBeans/ConfigBean.java
@@ -2,6 +2,7 @@ package com.personal.springcloud.cfgBeans;
 
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RetryRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +20,7 @@ public class ConfigBean {
     //当我们显示申明了负载均衡的算法，则会覆盖掉默认的轮询算法
     @Bean
     public IRule myRule(){
-        return new RandomRule();   //负载均衡的又一种算法：随机
+        //return new RandomRule();   //负载均衡的又一种算法：随机
+        return new RetryRule();   //重试算法：当服务的实例都正常，那么该算法和轮询算法一样，每个实例服务依次调用；当某一个实例宕机，那么它会重试，重试几次后，如果还是宕机状态，那么它就会在存活的正常实例中依次调用
     }
 }
